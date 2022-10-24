ce4b153d68e5f80031b9ab2abe3a79efc533c854
Marcel Baranek <marcel.baranek@tietoevry.com>
2022-10-24 21:47:29 +0200

    PUA

--- Semestr 5/PUA/Logical/Software/Code/Program/Types.typ
+++ Semestr 5/PUA/Logical/Software/Code/Program/Types.typ
@@ -3,12 +3,14 @@ TYPE
 		diToogle : ARRAY[1..2] OF BOOL; (* Wlaczniki *)
 		doLamp : ARRAY[1..2] OF BOOL; (* Lampy *)
 		aiTemperature : INT; (* Temperatura *)
+		diPhotoCom : ARRAY[1..2] OF BOOL; (* Fotokomorka *)
 	END_STRUCT;
 	
 	HouseStruct : STRUCT
 		Jadalnia : RoomStruct;
 		Kuchnia : RoomStruct;
 		Sypialnia : RoomStruct;
+		Korytarz : RoomStruct;
 	END_STRUCT;
 	
 END_TYPE
--- Semestr 5/PUA/Temp/Objects/Config1/X20CP1486/Libraries.h
+++ Semestr 5/PUA/Temp/Objects/Config1/X20CP1486/Libraries.h
@@ -1,8 +1,8 @@
 /* Automation Studio generated header file */
 /* Do not edit ! */
 
-#ifndef _LIBRARIES_20221016145714_
-#define _LIBRARIES_20221016145714_
+#ifndef _LIBRARIES_20221023124038_
+#define _LIBRARIES_20221023124038_
 
 __asm__(".section \".plc\"");
 
@@ -490,5 +490,5 @@ __asm__(".ascii \"iecfile \\\"Logical/Libraries/standard/standard.var\\\" scope 
 __asm__(".previous");
 
 
-#endif /* _LIBRARIES_20221016145714_ */
+#endif /* _LIBRARIES_20221023124038_ */
 
